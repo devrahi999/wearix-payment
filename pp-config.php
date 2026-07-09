9b016ecbf747345de1203d4544b90035047836ac
Md Rahi Rahman <mdrahirahman9999@gmail.com>
2026-07-10 04:23:54 +0600

    Add database configuration to pp-config.php

---
+++ pp-config.php
@@ -0,0 +1,8 @@
+<?php
+
+$db_host = getenv('DB_HOST') ?: '';
+$db_port = getenv('DB_PORT') ?: '';
+$db_user = getenv('DB_USER') ?: '';
+$db_pass = getenv('DB_PASSWORD') ?: '';
+$db_name = getenv('DB_NAME') ?: '';
+$db_prefix = getenv('DB_PREFIX') ?: 'pp_';
